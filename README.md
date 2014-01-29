ec270a701f5b4a762fcfd07ac3bd88ec1aee8133
Chris Wright <christopher.wright47@gmail.com>
2014-01-28 17:20:39 -0800

    modified readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #This is a list of my work on project euler problems
 
 christopher.wright47@gmail.com
+
 @chriswright47
--- prob_26.rb
+++
@@ -1,3 +0,0 @@
-# find the value of d within [1,1000) for which 1/d
-# contains the longest recurring cycle in its decimal fraction
-
