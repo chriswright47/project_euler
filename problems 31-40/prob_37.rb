41f87f45f5c4c047640ed742ed9c498d618984ab
Chris Wright <christopher.wright47@gmail.com>
2014-01-29 19:20:37 -0800

    started problem 37

---
+++ problems 31-40/prob_37.rb
@@ -0,0 +1,4 @@
+# find the sum of the only eleven numbers that are truncatable
+# from both right to left and left to right
+# (truncatable refers to the ability to remove one digit at a
+# time and still have a prime number, e.g. 3797)
